79e88dbf2145b4b3b9b9fb94db775df3c0bf2e40
NOTEBOOK-TANYA\ganzyukt <ganzyukr@gmail.com>
2022-07-07 17:48:07 +0300

    Homework task7

--- src/test/java/task7/QaAutomationStudent.java
+++ src/test/java/task7/QaAutomationStudent.java
@@ -1,6 +1,13 @@
 package task7;
 
 public class QaAutomationStudent extends Student{
+    public QaAutomationStudent(String name) {
+        super();
 
+    }
 
+    @Override
+    public void runHomeTask() {
+        System.out.println("Tatiana is coding");
+    }
 }
