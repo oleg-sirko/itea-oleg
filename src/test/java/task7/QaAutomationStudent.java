ff03f73ba29f91be8752adfe10eb51d1cc933427
DESKTOP-9SQCE4J\Vadym <vadym.m.taras@gmail.com>
2022-07-10 20:28:23 +0300

    HW7

--- src/test/java/task7/QaAutomationStudent.java
+++ src/test/java/task7/QaAutomationStudent.java
@@ -1,6 +1,11 @@
 package task7;
 
 public class QaAutomationStudent extends Student{
-
-
+public QaAutomationStudent(String name, boolean coding){
+    super(name,coding);
+}
+    @Override
+    public void runHomeTask() {
+        System.out.println(getName()+" is coding");
+    }
 }
