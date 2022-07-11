788261a2d6e0954362b022f70ef4c32784264410
NOTEBOOK-TANYA\ganzyukt <ganzyukr@gmail.com>
2022-07-11 23:52:07 +0300

    Homework task8

---
+++ src/test/java/task8/SearchResultsPage.java
@@ -0,0 +1,31 @@
+package task8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SearchResultsPage extends BasePage {
+
+    @FindBy (css = "div:nth-child(3) > .btn")
+    WebElement moreDetailsButton;
+
+    public SearchResultsPage(WebDriver driver) {
+        super (driver);
+    }
+
+    public void clickMoreDetailsButton () {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.elementToBeClickable(moreDetailsButton));
+        {
+            Actions builder = new Actions(driver);
+            builder.moveToElement(moreDetailsButton, 0, 0).perform();
+        }
+        moreDetailsButton.click();
+
+
+    }
+
+}
